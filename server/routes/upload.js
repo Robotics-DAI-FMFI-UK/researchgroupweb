0d713c43342a1a4d8aeaddb31b7f5c6a6d0ef05f
spirka3 <spirka3@uniba.sk>
2021-05-04 02:13:56 +0200

    odstaranenie komentaru

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -10,12 +10,12 @@ router.post("/", (req, res) => {
 
   const name = `[${Date.now()}]-${file.name.replace(/ /g, "-")}`;
   const path = `${__dirname}/../../client/public/uploads/${name}`;
+  //console.log(path);
 
-  return res.status(500).json({ message: "err.message" });
+  //return res.status(500).json({ message: "err.message" });
 
   file.mv(path, (err) => {
     if (err) return res.status(500).json({ message: err.message });
-
     res.json({
       success: 1,
       fileName: file.name,
